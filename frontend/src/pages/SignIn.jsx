5adde41e06a3891e20d20a86956eab1717836d0b
TanayUmre <tanayumre@gmail.com>
2026-09-16 00:46:23 +0530

    Added SignIn Page

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -9,8 +9,8 @@ function SignIn(){
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-        console.log("Email":{email});
-        console.log("Password":{password});
+        console.log("Email:",email);
+        console.log("Password:",password);
     };
 
     return (
